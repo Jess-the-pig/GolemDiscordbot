6d3961e2bdb0411a29c4827bd31642b3823a0737
Jess-the-pig <135271280+Jess-the-pig@users.noreply.github.com>
2025-07-15 11:41:06 +0200

    Setting up SoundCloud
    Cloning the handler for the app.
    tests with pom integration of the music dependency failed

--- GolemDiscordbot/src/main/java/Golem/api/ApiApplication.java
+++ GolemDiscordbot/src/main/java/Golem/api/ApiApplication.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 public class ApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(ApiApplication.class, args);
+  }
 }
--- GolemDiscordbot/src/main/java/Golem/api/commands/implementation/musicCommand.java
+++ GolemDiscordbot/src/main/java/Golem/api/commands/implementation/musicCommand.java
@@ -2,9 +2,15 @@ package Golem.api.commands.implementation;
 
 import Golem.api.commands.ICommand;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import org.springframework.web.client.RestTemplate;
 import reactor.core.publisher.Mono;
 
 public class musicCommand implements ICommand {
+  private RestTemplate restTemplate;
+
+  public musicCommand(RestTemplate restTemplate) {
+    this.restTemplate = restTemplate;
+  }
 
   @Override
   public String getName() {
